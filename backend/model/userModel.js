5622ea1f4003f1c9190982e0f6b4920a04bb271b
irfqn <irfanalrasyid96@gmail.com>
2024-06-12 19:37:45 +0700

    change bcrypt to bcryptjs

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -67,7 +67,7 @@
 // module.exports = mongoose.model("User", userSchema);
 
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
