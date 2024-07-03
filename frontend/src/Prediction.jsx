0552e24d7d16a5b99fd07c96570cff0d34e25701
irfqn <irfanalrasyid96@gmail.com>
2024-07-04 00:27:54 +0700

    change api url in prediction.jsx

--- frontend/src/Prediction.jsx
+++ frontend/src/Prediction.jsx
@@ -57,7 +57,7 @@ const Prediction = () => {
 
     try {
       setLoading(true);
-      const response = await fetch("https://bank-sampah-bersinar.onrender.com/api/predict", {
+      const response = await fetch("https://bank-sampah-bersinar-1.onrender.com/api/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
