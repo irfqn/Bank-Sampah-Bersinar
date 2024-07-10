34cfb64d84eba4a25e3aab3d0ab4e4efa3dc8f04
irfqn <irfanalrasyid96@gmail.com>
2024-07-10 12:32:59 +0700

    update url in trashdetaction

--- frontend/src/TrashDetaction.jsx
+++ frontend/src/TrashDetaction.jsx
@@ -68,7 +68,7 @@ const TrashDetection = () => {
       try {
         const currentDate = new Date();
         const currentMonth = currentDate.toISOString().slice(0, 7);
-        const response = await fetch(`http://localhost:3000/api/user/getPrice?month=${currentMonth}`);
+        const response = await fetch(`https://bank-sampah-bersinar-2.onrender.com/api/user/getPrice?month=${currentMonth}`);
         if (!response.ok) {
           throw new Error("Failed to fetch data prices");
         }
@@ -208,7 +208,7 @@ const getCookie = (name) => {
 const submitTotalHarga = async (totalHarga, detectedClasses) => {
   try {
     const token = getCookie("token");
-    const response = await fetch("http://localhost:3000/api/user/totalHarga", {
+    const response = await fetch("https://bank-sampah-bersinar-2.onrender.com/api/user/totalHarga", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
