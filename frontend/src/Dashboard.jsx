2eae81f4061ac008b6efe86021aeedf29f338a1f
irfqn <irfanalrasyid96@gmail.com>
2024-07-12 18:29:10 +0700

    update dashboard

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+// Dashboard page
 import './Dashboard.css';
 // import { Button } from './components/ui/button';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from './components/ui/card';
