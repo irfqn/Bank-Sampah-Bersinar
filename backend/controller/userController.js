95c678ba2b1766fe96da0f7d14e48f8f9cd18b44
irfqn <irfanalrasyid96@gmail.com>
2024-08-07 15:06:19 +0700

    undo userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,54 +1,47 @@
+
 const User = require("../model/userModel");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
-// Helper function to create a JWT token
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
-// Login controller
 exports.login = async (req, res) => {
   const { email, password } = req.body;
-
-  console.log('Login Request Body:', req.body); // Logging for debugging
-
   try {
     const user = await User.login(email, password);
 
-    // Sign in token
+    // sign in token
     const token = createToken(user._id);
 
     res.status(200).json({ email, token });
   } catch (error) {
-    console.error('Login Error:', error.message); // Logging for debugging
     res.status(400).json({ error: error.message });
   }
 };
 
-// Signup controller
+
 exports.signup = async (req, res) => {
   const { email, password, firstName, lastName, birthPlace, birthDate, phone, nik, address } = req.body;
-
-  console.log('Signup Request Body:', req.body); // Logging for debugging
 
   try {
     const user = await User.signup({ email, password, firstName, lastName, birthPlace, birthDate, phone, nik, address });
 
-    // Sign in token
+    // sign in token
     const token = createToken(user._id);
 
     res.status(200).json({ email, token });
   } catch (error) {
-    console.error('Signup Error:', error.message); // Logging for debugging
     res.status(400).json({ error: error.message });
   }
 };
 
-// Profile controller
 exports.profile = async (req, res) => {
   try {
     // Mendapatkan ID pengguna dari token yang divalidasi
     const userId = req.user._id;
+    // console.log(req.user)
 
     // Mengambil data profil pengguna berdasarkan ID-nya dari database
     const user = await User.findById(userId);
@@ -56,12 +49,10 @@ exports.profile = async (req, res) => {
     // Mengirim data profil pengguna sebagai respons
     res.json(user);
   } catch (error) {
-    console.error('Profile Error:', error.message); // Logging for debugging
     res.status(500).json({ message: error.message });
   }
 };
 
-// Get all users controller
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.find({}, "-password"); // Exclude the password field from the response
